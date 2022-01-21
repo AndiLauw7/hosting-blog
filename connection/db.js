044d1bb37b0df9b6457c205fbaac3fa76cc57295
andi lauw <andi.safariansyah7@gmail.com>
2022-01-21 16:56:54 +0700

    update

--- connection/db.js
+++ connection/db.js
@@ -5,6 +5,7 @@ const dbpool = new Pool({
     port: '5432',
     user: 'rkeayolfrekrda',
     password: '0610ed19423614997a5625be8ab3b48d78516404070353e12bc69e5c241bd8ee',
-    url: 'postgres://rkeayolfrekrda:0610ed19423614997a5625be8ab3b48d78516404070353e12bc69e5c241bd8ee@ec2-3-222-49-168.compute-1.amazonaws.com:5432/df1jkrk0941njo'
+    url: 'postgres://rkeayolfrekrda:0610ed19423614997a5625be8ab3b48d78516404070353e12bc69e5c241bd8ee@ec2-3-222-49-168.compute-1.amazonaws.com:5432/df1jkrk0941njo',
+    heroku_cli: 'heroku pg:psql postgresql-curly-92964 --app lauwsblogid'
 })
 module.exports = dbpool
