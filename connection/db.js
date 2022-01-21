74832447a521954ca2681927ba179e3dbd6bae9a
andi lauw <andi.safariansyah7@gmail.com>
2022-01-21 16:41:39 +0700

    update

--- connection/db.js
+++ connection/db.js
@@ -1,8 +1,9 @@
 const { Pool } = require('pg')
 const dbpool = new Pool({
-    database: 'postgres',
+    host: 'ec2-3-222-49-168.compute-1.amazonaws.com',
+    database: 'df1jkrk0941njo',
     port: '5432',
-    user: 'postgres',
-    password: '769800'
+    user: 'rkeayolfrekrda',
+    password: '0610ed19423614997a5625be8ab3b48d78516404070353e12bc69e5c241bd8ee'
 })
 module.exports = dbpool
